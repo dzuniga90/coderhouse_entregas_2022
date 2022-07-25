6d75d528878707449981e2cecccc5326c28d653c
dzuniga90 <75187867+dzuniga90@users.noreply.github.com>
2022-07-25 17:54:29 -0500

    Add files via upload

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import "../App.css";
+
+function Navbar() {
+    return (<nav>
+        <div className="storeLogo">Sweet Pix</div>
+        <div className="navlinks">
+        <a href="index.html">Top sellers</a>
+        <a href="index.html">Sweet</a>
+        <a href="index.html">Sour</a>
+        <a href="index.html">Gift Packs</a>
+        <a href="index.html">Chocolates</a></div>
+       </nav>);
+}
+
+export default Navbar;
