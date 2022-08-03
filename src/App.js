57f60fd040ad0737bbb4a552daa9c698e8f04977
dzuniga90 <75187867+dzuniga90@users.noreply.github.com>
2022-08-03 10:28:39 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import ItemListContainer from "./components/container/container";
+import ItemListContainer from "./components/container/listcontainer";
 import Navbar from "./components/navbar";
 import CartWidget from "./components/cart"
 import Counter from "./components/contador/index";
-import ItemList from "./components/ItemList/itemlist";
 
 function App() {
   return (
@@ -16,7 +15,8 @@ function App() {
       </header>
 
       <Counter></Counter>
-      <ItemList/>
+      <h2>Best Sellers</h2>
+      <ItemListContainer/>      
       {/*
       <ItemListContainer texto={'Find the best sweets here '}></ItemListContainer>
       <ItemListContainer texto={'A subscription candy box service for your taste buds'}></ItemListContainer>
