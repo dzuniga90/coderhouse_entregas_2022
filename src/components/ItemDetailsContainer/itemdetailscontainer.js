854113a7ec0ab44acc327d3fbef8b3fca7b0ec7f
dzuniga90 <75187867+dzuniga90@users.noreply.github.com>
2022-08-09 19:46:27 -0500

    Add files via upload

---
+++ src/components/ItemDetailsContainer/itemdetailscontainer.js
@@ -0,0 +1,28 @@
+import {useState, useEffect} from "react";
+import {getProductById} from "../data/data";
+import ItemDetails from "../Item/ItemDetails.js/itemdetails";
+import { useParams } from 'react-router-dom';
+
+function ItemDetailsContainer() {
+const [product, setProduct] = useState({});
+    const {productID} = useParams();
+
+    useEffect(() => {
+        getProductById(productID).then(
+            products => {
+                console.log("products >>> " + products)
+                setProduct(products);
+            }
+        );
+        console.log("productId " + productID);
+
+    }, [productID])
+    
+
+    return (<>
+        <h1>Details</h1>
+        <ItemDetails {...product}/>
+    </>);
+}
+
+export default ItemDetailsContainer;
