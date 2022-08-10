ce179ffc0d90df400cc99808f7963c2da4e97dbd
dzuniga90 <75187867+dzuniga90@users.noreply.github.com>
2022-08-09 19:43:48 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import ItemListContainer from "./components/container/listcontainer";
+import ItemListContainer from "./components/ItemListContainer/listcontainer";
 import Navbar from "./components/navbar";
-import CartWidget from "./components/cart"
-import Counter from "./components/contador/index";
-
+import Counter from "./components/Counter/index";
+import { Routes, Route} from "react-router-dom";
+import ItemDetailsContainer from "./components/ItemDetailsContainer/itemdetailscontainer";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Navbar/>
-         <CartWidget/>
-
-      </header>
-
-      <Counter></Counter>
+    
+      <Navbar/>
+      {/*<Counter></Counter>*/}
       <h2>Best Sellers</h2>
-      <ItemListContainer/>      
-      {/*
-      <ItemListContainer texto={'Find the best sweets here '}></ItemListContainer>
-      <ItemListContainer texto={'A subscription candy box service for your taste buds'}></ItemListContainer>
-      <ItemListContainer texto={'Rated best in variety and quality'}></ItemListContainer>  */}
+      <Routes>
+        <Route path='/' element={<ItemListContainer/>}/>
+        <Route path='/category/:categoryID' element={<ItemListContainer/>}/>
+        <Route path='/detail/:productID' element={<ItemDetailsContainer/>} />
+      </Routes>
+
     </div>
   );
 }
