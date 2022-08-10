52d3652d145298592d47cb22799c264d4f675537
dzuniga90 <75187867+dzuniga90@users.noreply.github.com>
2022-08-09 19:49:32 -0500

    Add files via upload

--- src/components/ItemListContainer/listcontainer.js
+++ src/components/ItemListContainer/listcontainer.js
@@ -1,19 +1,29 @@
 import {useState, useEffect} from "react";
-import getFetch from "../data/data";
+import {getProductsByCategory, getProducts} from "../data/data";
 import ItemList from "../ItemList/itemlist";
+import { useParams } from 'react-router-dom';
 
-function ItemListContainer(props) {
-    const [data, setData] = useState([]);
-
+function ItemListContainer() {
+    const [products, setProducts] = useState([]);
+    const {categoryID} = useParams();
     useEffect(() => {
-        getFetch
-        .then((resp)=>setData(resp))
+    if(!categoryID) {
+        getProducts().then(products=> {
+            setProducts(products)
+        })
         .catch(err=>console.log(err))
-    },[])
+    }
+    else {
+        getProductsByCategory(categoryID).then((products) => {
+            setProducts(products)
+        })
+    }
+    },[categoryID])
     
-    return (<>
-        <ItemList props={data}/>
-    </>);
+    return (<div className="products">
+        <ItemList props={products}/>
+        </div>
+    );
 }
 
 export default ItemListContainer;
