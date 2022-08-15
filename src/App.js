99534d00bc69c49187d127e070771dd5df740a72
dzuniga90 <75187867+dzuniga90@users.noreply.github.com>
2022-08-14 22:33:19 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import ItemListContainer from "./components/ItemListContainer/listcontainer";
 import Navbar from "./components/navbar";
-import Counter from "./components/Counter/index";
-import { Routes, Route} from "react-router-dom";
+import Counter from "./components/counter/index";
+import Cart from "./components/Cart/cart";
+import { Routes, Route, BrowserRouter} from "react-router-dom";
 import ItemDetailsContainer from "./components/ItemDetailsContainer/itemdetailscontainer";
+import CartCustomContext from "./components/CartContext/cartcontext";
+
 function App() {
   return (
     <div className="App">
-    
+    <CartCustomContext>
+    <BrowserRouter>
       <Navbar/>
       {/*<Counter></Counter>*/}
       <h2>Best Sellers</h2>
       <Routes>
         <Route path='/' element={<ItemListContainer/>}/>
+        <Route path='/cart' element={<Cart/>}/>
         <Route path='/category/:categoryID' element={<ItemListContainer/>}/>
         <Route path='/detail/:productID' element={<ItemDetailsContainer/>} />
       </Routes>
-
+    </BrowserRouter>
+    </CartCustomContext>
     </div>
   );
 }
