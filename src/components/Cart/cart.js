5b101fec0feefc49cde2ae8940b6122dc669fb98
dzuniga90 <75187867+dzuniga90@users.noreply.github.com>
2022-08-14 22:38:26 -0500

    Add files via upload

---
+++ src/components/Cart/cart.js
@@ -0,0 +1,22 @@
+import React, {useContext} from "react";
+//import { CartContext } from '../CartContext/cartcontext';
+
+
+function Cart () {
+    //const {cartData} = useContext(CartContext);
+
+    //console.log("cart data >>> ", cartData);
+
+return (    
+    <div className="cartInfo">
+    <h3>Cart info</h3>
+    {/*
+        cartData.map((item) => {
+            return <div>{`ID: ${item.id} - Quantity ${item.quantity}`}</div>;
+        }) */
+    }
+    </div>
+)
+}
+
+export default Cart;
