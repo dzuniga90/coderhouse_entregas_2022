e1f373b84f2410814c7ebb4316c420c14c111263
dzuniga90 <75187867+dzuniga90@users.noreply.github.com>
2022-08-17 21:15:58 -0500

    Add files via upload

--- src/components/Cart/cart.js
+++ src/components/Cart/cart.js
@@ -1,19 +1,54 @@
 import React, {useContext} from "react";
 import { CartContext } from '../CartContext/cartcontext';
+import {useState} from "react";
 
 
 function Cart () {
     const {cartData} = useContext(CartContext);
-
     console.log("cart data >>> ", cartData);
+
+    const {clearCart} = useContext(CartContext);
+    const {removeFromCart} = useContext(CartContext);
+    const [removedFromCart, setRemovedFromCart] = useState(false);
+    const [confrimDelete, setConfirmDelete] = useState(false);
+
+    const onDelete = (candyID) => {
+        removeFromCart(candyID);
+        setRemovedFromCart(true);
+        console.log("Removed from cart: >>> ", removedFromCart);
+    }
 
 return (    
     <div className="cartInfo">
     <h3>Cart info</h3>
-    {/*
+    <button onClick={clearCart}>Clear Cart</button>
+
+    {
         cartData.map((item) => {
-            return <div>{`ID: ${item.id} - Quantity ${item.quantity}`}</div>;
-        }) */
+            return <div>
+                <div className="itemQuantity">
+                    <h2>
+                {item.counter}
+                    </h2>
+                </div>
+                <div className="cartDetail">
+                <div className="items">
+                    <div className="itemImg">
+                    <img src={item.pictureUrl} />
+                    </div>
+                    <div className="cardBody">
+                    {item.title}<br></br>
+                    <br></br>
+                    <div className="cardText">
+                     {item.description}<br></br>
+                     {"$" + item.price}
+                    </div> 
+                    <button onClick={() => onDelete(item.candyID) }>Remove</button>           
+                    </div>
+                </div>
+                </div>
+            </div>;
+        })
     }
     </div>
 )
