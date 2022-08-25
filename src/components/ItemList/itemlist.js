3ace46f6df4cf86330ae40bb4beb9004995289d0
dzuniga90 <75187867+dzuniga90@users.noreply.github.com>
2022-08-24 21:17:03 -0500

    Add files via upload

--- src/components/ItemList/itemlist.js
+++ src/components/ItemList/itemlist.js
@@ -2,13 +2,13 @@ import Item from "../Item/index";
 function ItemList({props}) {
     return (
      props.map(item => {
-        console.log(item.candyID);
+        console.log(item.categoryID);
         console.log(item.title);
         console.log(item.price);
         console.log(item.pictureUrl);
         return (<div className="itemList">
-            <Item key={item.candyID} id={item.candyID} categoryId = {item.categoryID} title={item.title} price={item.price}
-            picURL = {item.pictureUrl} desc= {item.description}/>
+            <Item key={item.id} id={item.id} categoryId = {item.categoryID} title={item.title} price={item.price}
+            picURL = {item.picURL} desc= {item.description}/>
         </div>);
     }
     ));
