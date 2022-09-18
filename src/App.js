da31bf2b6ee5e5eab0a3713968e0a5dc9d539c6f
dzuniga90 <75187867+dzuniga90@users.noreply.github.com>
2022-09-18 18:46:29 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,23 +5,27 @@ import Cart from "./components/Cart/cart";
 import { Routes, Route, BrowserRouter} from "react-router-dom";
 import ItemDetailsContainer from "./components/ItemDetailsContainer/itemdetailscontainer";
 import CartCustomContext from "./components/CartContext/cartcontext";
-
+import SignUp from './components/SignUp/signup';
+import AuthProvider from './components/context/authcontext';
 
 function App() {
   return (
     <div className="App">
     <CartCustomContext>
+    <AuthProvider>
     <BrowserRouter>
       <Navbar/>
-      {/*<Counter></Counter>*/}
-      <h2>Best Sellers</h2>
+      {/*<Counter></Counter>
+      <h2>Best Sellers</h2> */}
       <Routes>
         <Route path='/' element={<ItemListContainer/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/category/:categoryID' element={<ItemListContainer/>}/>
         <Route path='/detail/:productID' element={<ItemDetailsContainer/>} />
       </Routes>
     </BrowserRouter>
+    </AuthProvider>
       </CartCustomContext>
     </div>
   );
