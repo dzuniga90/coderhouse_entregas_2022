e11483444b697395a300d0377b9a646744df8d7a
dzuniga90 <75187867+dzuniga90@users.noreply.github.com>
2022-09-18 18:55:30 -0500

    Add files via upload

--- src/components/Cart/cart.js
+++ src/components/Cart/cart.js
@@ -2,18 +2,23 @@ import React, {useContext} from "react";
 import { CartContext } from '../CartContext/cartcontext';
 import {useState} from "react";
 
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import CartModal from '../CartModal/modal';
 function Cart () {
-    const {cartData} = useContext(CartContext);
-    console.log("cart data >>> ", cartData);
+    const {cart} = useContext(CartContext);
+    console.log("cart data >>> ", cart);
 
     const {clearCart} = useContext(CartContext);
     const {removeFromCart} = useContext(CartContext);
     const [removedFromCart, setRemovedFromCart] = useState(false);
     const [confrimDelete, setConfirmDelete] = useState(false);
+    const [showModal, setShowModal] = useState(false);
 
-    const onDelete = (candyID) => {
-        removeFromCart(candyID);
+    const onDelete = (id) => {
+        setShowModal(true);
+        removeFromCart(id);
         setRemovedFromCart(true);
         console.log("Removed from cart: >>> ", removedFromCart);
     }
@@ -21,11 +26,19 @@ function Cart () {
 return (    
     <div className="cartInfo">
     <h3>Cart info</h3>
-    <button onClick={clearCart}>Clear Cart</button>
+
+    {/*<button onClick={clearCart}>Clear Cart</button> */}
+    <div className="clearCart">
+        <Button variant="danger" onClick={clearCart} size='md'>Clear Cart</Button>
+    </div>
 
     {
-        cartData.map((item) => {
-            return <div>
+        cart.map((item) => {
+
+            return <div className="cartContainer">
+                        {showModal && <CartModal title="Are you sure you want to delete this item?" 
+                        action="Delete" cancelAction="Cancel" handleAction={() => onDelete(item.id)} handleClose={() => setShowModal(false)}
+                         />}
                 <div className="itemQuantity">
                     <h2>
                 {item.counter}
@@ -34,7 +47,9 @@ return (
                 <div className="cartDetail">
                 <div className="items">
                     <div className="itemImg">
-                    <img src={item.pictureUrl} />
+                    <img src={item.picURL}/>
+                    <button class="btn btn-secondary btn-lg" onClick={() => setShowModal(true) }><i class="bi bi-trash"></i></button>           
+
                     </div>
                     <div className="cardBody">
                     {item.title}<br></br>
@@ -43,7 +58,6 @@ return (
                      {item.description}<br></br>
                      {"$" + item.price}
                     </div> 
-                    <button onClick={() => onDelete(item.candyID) }>Remove</button>           
                     </div>
                 </div>
                 </div>
