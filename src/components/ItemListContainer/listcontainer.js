74ae0cc1c7089786275290c3f5b245d018de4751
Darwin Zuniga <75187867+dzuniga90@users.noreply.github.com>
2022-10-14 22:06:58 -0500

    Add files via upload

--- src/components/ItemListContainer/listcontainer.js
+++ src/components/ItemListContainer/listcontainer.js
@@ -6,18 +6,21 @@ import { useParams } from 'react-router-dom';
 function ItemListContainer() {
     const [products, setProducts] = useState([]);
     const {categoryID} = useParams();
+
     useEffect(() => {
-    if(!categoryID) {
-        getProducts().then(result=> {
-            setProducts(result)
-        })
-        .catch(err=>console.log(err))
-    }
-    else {
-        getProductsByCategory(categoryID).then((result) => {
-            setProducts(result)
-        })
-    }
+        if(!categoryID) {
+            getProducts().then(result=> {
+                setProducts(result)
+            })
+            .catch(err=>console.log(err))
+        }
+        else {            
+            getProductsByCategory(categoryID).then(result => {
+            
+                setProducts(result);
+            })
+            .catch(err => alert("category not found")) 
+        }
     },[categoryID])
     
     return (<div className="products">
