bfeecd158fb77af18e55f6fe2eb1c24c3a4f0346
Darwin Zuniga <75187867+dzuniga90@users.noreply.github.com>
2022-10-14 22:01:42 -0500

    Add files via upload

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 function Item(props) {
     return (
         <div className="items">
-            <div class="itemImg">
+            <div className="itemImg">
             <img src={props.picURL} />
             </div>
             <div className="cardBody">
